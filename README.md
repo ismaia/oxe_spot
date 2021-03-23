0d25416fb582e7056a415ca44e7d0e513461805c
Isaac Maia Pessoa <526473+gb96@users.noreply.github.com>
2021-03-23 20:09:55 +0100

    first draft

---
+++ README.md
@@ -0,0 +1,9 @@
+## Oxe Spot
+
+My personal audio home system
+  * Devices 
+    * 2x BT speakers
+    * A Turntable
+    * SmartPhone (Audio Source)
+
+
---
+++ bluetooth/bt_scan_test.py
@@ -0,0 +1,71 @@
+import logging
+
+from bluezero import adapter
+from bluezero import device
+from bluezero import tools
+
+
+def dev_added_callback(dev):
+    print(dev.address , " " , dev.alias)
+
+def main():
+    adapter_list = adapter.list_adapters() 
+    for a in adapter_list:
+       adpt = adapter.Adapter(a)
+       print('address: ', adpt.address)
+       print('name: ', adpt.name)
+       print('alias: ', adpt.alias)
+       print('powered: ', adpt.powered)
+       print('pairable: ', adpt.pairable)
+       print('pairable timeout: ', adpt.pairabletimeout)
+       print('discoverable: ', adpt.discoverable)
+       print('discoverable timeout: ', adpt.discoverabletimeout)
+       print('discovering: ', adpt.discovering)
+       print('Powered: ', adpt.powered)
+       if not adpt.powered:
+           adpt.powered = True
+           print('Now powered: ', adpt.powered)
+       print('Start discovering')
+    
+    adapter0 = adapter.Adapter(adapter_list[0])
+    adapter0.on_device_found = dev_added_callback
+    adapter0.nearby_discovery(timeout=15)
+    adapter0.alias = 'Inspi'
+
+
+    print('Nearby Devices:')
+    devs = device.Device.available()
+    for d in devs:
+        print(d.address , ' ' , d.alias)
+        if d.alias == 'YET-M1':
+          yet_m1_device = d
+        if d.alias == 'MEGABOOM 3':
+          megaboom3_device = d
+
+        
+    for i in range(4):
+       if yet_m1_device.paired:
+           print('yet_m1 paired')
+           yet_m1_device.trusted = True
+           break
+       else:
+           print('trying to pair yet_m1, attempt ' , i) 
+           yet_m1_device.pair()
+
+    for i in range(4):
+       if megaboom3_device.paired:
+           print('megaboom3 paired')
+           megaboom3_device.trusted = True
+           megaboom3_device.connect()
+           break
+       else:
+           print('trying to pair megaboom3, attempt ' , i) 
+           megaboom3_device.pair()
+
+
+
+if __name__ == '__main__':
+    print(__name__)
+    logger = tools.create_module_logger('adapter')
+    logger.setLevel(logging.DEBUG)
+    main()
