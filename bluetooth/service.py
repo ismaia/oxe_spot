ab1061f0de916dc6c652ddd8dd5b7e6db547db45
Isaac Maia <5jhjfsd26jshfj473skhfs+gbjadha96@users.noreply.github.com>
2021-03-26 23:05:36 +0100

    set_adapter_alias working

--- bluetooth/service.py
+++ bluetooth/service.py
@@ -1,39 +1,106 @@
 import dbus
 from gi.repository import GLib
+import time
 
 from bluezero import adapter
 from bluezero import device
 from bluezero import tools
 
+
+
 class BtService:
 
-    def __init__(self):
-        self.adapter_list = []        
-        self.adapter_dict = {}
+    def __init__(self):               
+        self.adapter_alias_dict = {}
+        self.adapter_addr_dict = {}
         for a in adapter.list_adapters(): 
             ad = adapter.Adapter(a)
-            self.adapter_list.append(ad)
-        self.device_list = list(device.Device.available())
+            self.adapter_alias_dict[ad.alias] = ad
+            self.adapter_addr_dict[ad.address] = ad
+            ad.on_device_found = self._on_device_found
+            ad.on_device_found = self._on_device_connect
+            ad.on_device_found = self._on_device_disconnect
+            
+        self.device_alias_dict = {}
+        self.device_addr_dict = {}
+        for d in list(device.Device.available()):
+            self.device_alias_dict[d.alias] = d
+            self.device_addr_dict[d.address] = d
 
     
-    def availabe_devices(self):
-        return self.device_list
 
-    def set_adapter_name(self, hci_name, name):
-        for ad in self.adapter_list:
-            if hci_name in ad.path:
-                ad.alias = name
-                self.adapter_dict[ad.alias] = ad
+    def set_adapter_alias(self, hci_name, alias):
+        for ad in self.adapter_addr_dict.values():
+            if hci_name in ad.path:                    
+                old_ad_alias = ad.alias
+                if old_ad_alias in self.adapter_alias_dict:
+                    del self.adapter_alias_dict[old_ad_alias]
+                    self.adapter_alias_dict[alias] = ad
+                ad.alias = alias #set its new name
+                time.sleep(1) #wait dbus transaction
+                break
+        
+
+    def list_available_adapters(self):
+        for a in self.adapter_addr_dict.values():            
+            print( a.alias ,'(' , a.address ,')' )
 
     def list_available_devices(self):
-        for d in self.device_list:
-            print('[', d.address, ' - ', d.name ,']')
+        for d in self.device_addr_dict.values():
+            if d.adapter in self.adapter_addr_dict:
+               ad_alias = self.adapter_addr_dict[d.adapter].alias
+               print( d.alias ,'(' , d.address ,') => ' , ad_alias, '(' , d.address , ')'  )
+            else:
+               print( d.alias ,'(' , d.address ,') => (' , d.address , ')' )
     
-    def connect_device(self, dev_name, adapter_name):
-        for d in self.device_list:
-            if d.alias == dev_name:
-                print('Connecting device [', dev_name ,' - ', d.address , '] ...')
-                if not d.paired:
-                    pass
+    def connect_device(self, dev_id, adapter_alias):
+        adapter = None
+        if adapter_alias in self.adapter_alias_dict:
+            adapter = self.adapter_alias_dict[adapter_alias]
+            print('Connecting device :', dev_id , '=>' ,  adapter.alias , '(' , adapter.address , ')' )
+        else:
+            print('Error: Adapter not found: ', adapter_alias)
+            return
+        print(self.device_alias_dict[dev_id].adapter , adapter.address )
+
+        
+        if self.device_alias_dict[dev_id].adapter == adapter.address:
+          print('Device', dev_id , 'is available on ' , adapter.alias)
+        else:
+          print('Device', dev_id , 'is not available on ' , adapter.alias)
+          self._discovery(dev_id, adapter_alias)
+          
+
+    def _discovery(self, dev_id, adapter_alias):
+        if adapter_alias not in self.adapter_alias_dict:
+            print('Adapter not found:',  adapter_alias)
+            return
+        print('Discover :' ,dev_id)
+        ad = self.adapter_alias_dict[adapter_alias]
+        ad.nearby_discovery()
+        
+
+        
+                
+    def _on_device_found(self,dev):
+        print('on_device_found : ', dev.alias, dev.address)
+
+    def _on_device_connect(self,dev):
+        pass
+
+    def _on_device_disconnect(self,dev):
+        pass
+
+
+        # for d in self.device_list:
+        #     if d.alias == dev_id:
+        #         print('Connecting device [', dev_id ,' - ', d.address , '] ...')
+        #         if not d.paired:
+        #             pass
+
+           
+
+
+
 
 
--- oxe_spot.py
+++ oxe_spot.py
@@ -1,24 +1,73 @@
+import sys
 import dbus
+import signal
+import time
 from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+
+
 from bluetooth.service import BtService
+
+def signal_handler(sig, frame):
+    print('done')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def test_adapter_rename():
+    bt_adapter_main='ad_main1'
+    bt_adapter_a2dp_port1='a2dp_port1'
+
+    print('available adapters:')
+    bt_service.list_available_adapters()    
+    print('renaming adapters...')
+    bt_service.set_adapter_alias('hci0', bt_adapter_main)
+    bt_service.set_adapter_alias('hci1', bt_adapter_a2dp_port1)    
+    print('adapters after rename:')
+    bt_service.list_available_adapters()    
+    print('devices list:')
+    bt_service.list_available_devices()    
+    print('--')
+    print('--')
+
+    bt_adapter_main='ad_main2'
+    bt_adapter_a2dp_port1='a2dp_port2'
+
+    print('available adapters:')
+    bt_service.list_available_adapters()    
+    print('renaming adapters...')
+    bt_service.set_adapter_alias('hci0', bt_adapter_main)
+    bt_service.set_adapter_alias('hci1', bt_adapter_a2dp_port1)    
+    print('adapters after rename:')
+    bt_service.list_available_adapters()    
+    print('devices list:')
+    bt_service.list_available_devices()    
+    print('--')
+
+
 
 if __name__ == '__main__':
     
     print('OXE Spot\n')
+    DBusGMainLoop(set_as_default=True)
 
     bt_adapter_main='oxe_spot'
-    bt_adapter_a2dp='a2dp_sink'
+    bt_adapter_a2dp_port1='a2dp_port1'
+
     bt_service = BtService()
-    bt_service.set_adapter_name('hci0', bt_adapter_main)
-    bt_service.set_adapter_name('hci1', bt_adapter_a2dp)
 
+    bt_service.set_adapter_alias('hci0', bt_adapter_main)
+    bt_service.set_adapter_alias('hci1', bt_adapter_a2dp_port1)    
+    print('adapters:')
+    bt_service.list_available_adapters()    
+    print('devices list:')
     bt_service.list_available_devices()    
-    bt_service.connect_device('SoundCore 2', bt_adapter_a2dp)
+    print('--')
+
+#    bt_service.connect_device('SoundCore 2',bt_adapter_a2dp_port1)
 
 
-
-
-    # loop = GLib.MainLoop()
-    # loop.run()    
+    loop = GLib.MainLoop()
+    loop.run()    
 
     
